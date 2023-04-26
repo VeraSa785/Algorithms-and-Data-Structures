7220eaae7e117ebf26864ca9ebdb52c4a8617ddd
verasazonova <vera.zhokhova@gmail.com>
2023-04-26 14:48:01 -0700

    Add two methods: append and print linked list

--- EXERCISE-LL-Constructor.py
+++ EXERCISE-LL-Constructor.py
@@ -2,30 +2,31 @@ class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
-
         
 class LinkedList:
     def __init__(self, value):
-
         new_node = Node(value)
         self.head = new_node
         self.tail = new_node
         self.length = 1
 
+    def print_list(self):
+        node = self.head
+        while node is not None:
+            print(node.value)
+            node = node.next
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node 
+            self.tail = new_node 
+        self.length += 1
+
 my_linked_list = LinkedList(4)
-
-print('Head:', my_linked_list.head.value)
-print('Tail:', my_linked_list.tail.value)
-print('Length:', my_linked_list.length)
-
-
-"""
-    EXPECTED OUTPUT:
-    ----------------
-    Head: 4
-    Tail: 4
-    Length: 1
-    
-"""
-
+my_linked_list.append(3)
+my_linked_list.print_list()
                                                                                                                     
