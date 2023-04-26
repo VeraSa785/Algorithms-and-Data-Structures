435f2c7829b7cadc65a0b03e7071a18f7c7459c4
verasazonova <vera.zhokhova@gmail.com>
2023-04-26 15:21:29 -0700

    Add pop method to the linked list

--- EXERCISE-LL-Constructor.py
+++ EXERCISE-LL-Constructor.py
@@ -26,7 +26,30 @@ class LinkedList:
             self.tail = new_node 
         self.length += 1
 
+    def pop(self):
+        if self.length == 0:
+            return None
+        
+        temp = self.head
+        pre = self.head
+
+        while (temp.next):
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.next = None
+        self.length -= 1
+
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp.value
+
 my_linked_list = LinkedList(4)
 my_linked_list.append(3)
-my_linked_list.print_list()
+# my_linked_list.print_list()
+
+print(my_linked_list.pop())
+print(my_linked_list.pop())
+print(my_linked_list.pop())
                                                                                                                     
