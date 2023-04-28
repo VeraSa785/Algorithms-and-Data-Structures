2b21a755c29f131e1f2dba9dc30b7527d4410fb5
verasazonova <vera.zhokhova@gmail.com>
2023-04-28 14:52:51 -0700

    Test

--- EXERCISE-LL-Constructor.py
+++ EXERCISE-LL-Constructor.py
@@ -1,65 +1,92 @@
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.next = None
+print("dddd")
+# class Node:
+#     def __init__(self, value):
+#         self.value = value
+#         self.next = None
         
-class LinkedList:
-    def __init__(self, value):
-        new_node = Node(value)
-        self.head = new_node
-        self.tail = new_node
-        self.length = 1
+# class LinkedList:
+#     def __init__(self, value):
+#         new_node = Node(value)
+#         self.head = new_node
+#         self.tail = new_node
+#         self.length = 1
 
-    def print_list(self):
-        node = self.head
-        while node is not None:
-            print(node.value)
-            node = node.next
+#     def print_list(self):
+#         node = self.head
+#         while node is not None:
+#             print(node.value)
+#             node = node.next
 
-    def append(self, value):
-        new_node = Node(value)
-        if self.length == 0:
-            self.head = new_node
-            self.tail = new_node
-        else:
-            self.tail.next = new_node 
-            self.tail = new_node 
-        self.length += 1
+    # def append(self, value):
+    #     new_node = Node(value)
+    #     if self.length == 0:
+    #         self.head = new_node
+    #         self.tail = new_node
+    #     else:
+    #         self.tail.next = new_node 
+    #         self.tail = new_node 
+    #     self.length += 1
 
-    def pop(self):
-        if self.length == 0:
-            return None
+    # def pop(self):
+    #     if self.length == 0:
+    #         return None
         
-        temp = self.head
-        pre = self.head
+    #     temp = self.head
+    #     pre = self.head
 
-        while (temp.next):
-            pre = temp
-            temp = temp.next
-        self.tail = pre
-        self.next = None
-        self.length -= 1
+    #     while (temp.next):
+    #         pre = temp
+    #         temp = temp.next
+    #     self.tail = pre
+    #     self.next = None
+    #     self.length -= 1
 
-        if self.length == 0:
-            self.head = None
-            self.tail = None
-        return temp.value
+    #     if self.length == 0:
+    #         self.head = None
+    #         self.tail = None
+    #     return temp.value
     
-    def prepend(self, value):
-        new_node = Node(value)
-        if self.length == 0:
-            self.head = new_node
-            self.tail = new_node
-        else:
-            new_node.next = self.head
-            self.head = new_node
-            self.length += 1
+    # def prepend(self, value):
+    #     new_node = Node(value)
+    #     if self.length == 0:
+    #         self.head = new_node
+    #         self.tail = new_node
+    #     else:
+    #         new_node.next = self.head
+    #         self.head = new_node
+    #         self.length += 1
 
-my_linked_list = LinkedList(4)
-my_linked_list.append(3)
+    # def pop_first(self):
+    #     if self.length == 0:
+    #         return None
+        
+    #     temp = self.head
+    #     self.head = temp.next
+    #     self.next = None
+    #     self.length -= 1
+    #     if self.length == 0:
+    #         self.tail = None
+    #     return temp.value
+    
+    # def get(self, index):
+    #     if index < 0 or index >= self.length:
+    #         return None
+    #     temp = self.head
+    #     for _ in range(index):
+    #         temp = self.next 
+    #     return temp.value
+
+# my_linked_list = LinkedList(4)
+# my_linked_list.append(3)
 # my_linked_list.print_list()
 # print(my_linked_list.pop())
 # print(my_linked_list.pop())
 # print(my_linked_list.pop())
-my_linked_list.prepend(15)
-my_linked_list.print_list()                                                                                                        
+# my_linked_list.prepend(15)
+# my_linked_list.print_list()     
+# print(my_linked_list.pop_first())
+# print(my_linked_list.pop_first())
+# print(my_linked_list.pop_first())
+# print(my_linked_list.get(2))
+# print(my_linked_list.get(0))   
+# print(my_linked_list.get(-1))                                                                           
