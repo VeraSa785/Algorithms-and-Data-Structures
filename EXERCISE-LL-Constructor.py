9600d4d9d5b578fe3b9762e55ae2beb02ede8f08
verasazonova <vera.zhokhova@gmail.com>
2023-05-02 14:44:09 -0700

    Add remove and insert methods

--- EXERCISE-LL-Constructor.py
+++ EXERCISE-LL-Constructor.py
@@ -80,6 +80,42 @@ class LinkedList:
             temp.value = value
             return True
         return False
+    
+    def insert_value(self,index,value):
+
+        if index < 0 or index >= self.length:
+            return False
+
+        if index == 0:
+            return self.prepend(value)
+
+        if index == self.length:
+            return self.append(value)
+        
+        new_node = Node(value)
+        temp = self.get(index-1)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1
+        return True
+    
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length:
+            return self.pop()
+        
+        pre = self.get(index-1)
+        temp = pre.next
+        pre.next = temp.next
+        temp.next = None
+
+        self.length -= 1
+        return temp
+
+
 
 my_linked_list = LinkedList(11)
 my_linked_list.append(3)
@@ -97,5 +133,7 @@ my_linked_list.append(7)
 # print(my_linked_list.get(2))
 # print(my_linked_list.get(0))   
 # print(my_linked_list.get(-1))
-my_linked_list.set_value(1,4)
+# my_linked_list.set_value(1,4)
+# my_linked_list.insert_value(1,1)
+print(my_linked_list.remove(2))
 my_linked_list.print_list()                                                                           
